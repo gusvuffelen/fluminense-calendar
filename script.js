f8e791b4790e99452bfd18912be7e67240f8b9e9
Gustavo <gusvuffelen@gmail.com>
2023-04-17 13:03:08 -0300

    feat: traduz fullcalendar e melhora cores

--- script.js
+++ script.js
@@ -99,7 +99,72 @@ $(document).ready(async function () {
     editable: false,
     minTime: '7:30:00',
     maxTime: '21:30:00',
-    contentHeight: 500
+    contentHeight: 500,
+    /** */
+    ignoreTimezone: false,
+    monthNames: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro'
+    ],
+    monthNamesShort: [
+      'Jan',
+      'Fev',
+      'Mar',
+      'Abr',
+      'Mai',
+      'Jun',
+      'Jul',
+      'Ago',
+      'Set',
+      'Out',
+      'Nov',
+      'Dez'
+    ],
+    dayNames: [
+      'Domingo',
+      'Segunda',
+      'Terça',
+      'Quarta',
+      'Quinta',
+      'Sexta',
+      'Sabado'
+    ],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    /*titleFormat: {
+      month: 'MMMM yyyy',
+      week: 'd[ MMMM][ yyyy]{ - d MMMM yyyy}',
+      day: 'dddd, d MMMM yyyy'
+    },
+    columnFormat: {
+      month: 'ddd',
+      week: 'ddd d',
+      day: ''
+    },
+    axisFormat: 'H:mm',
+    timeFormat: {
+      '': 'H:mm',
+      agenda: 'H:mm{ - H:mm}'
+    },*/
+    buttonText: {
+      prev: ' < ',
+      next: ' > ',
+      prevYear: '&nbsp;&lt;&lt;&nbsp;',
+      nextYear: '&nbsp;&gt;&gt;&nbsp;',
+      today: 'Hoje',
+      month: 'Mês',
+      week: 'Semana',
+      day: 'Dia'
+    }
   });
   const url_feed = URIHash.get('feed');
   const url_file = URIHash.get('file');
